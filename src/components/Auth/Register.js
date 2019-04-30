4319f8d654a99db5b4c19addc3b5e2c015c9dc26
raymondsjkim <raymond.sj.kim@gmail.com>
2019-04-30 15:49:44 -0700

    form validation for register

--- src/components/Auth/Register.js
+++ src/components/Auth/Register.js
@@ -9,8 +9,46 @@ class Register extends React.Component {
         username: '',
         email: '',
         password: '',
-        passwordConfirmation: ''
+        passwordConfirmation: '',
+        errors: []
     };
+
+    isFormValid = () => {
+        let errors = [];
+        let error;
+
+        if(this.isFormEmpty(this.state)) {
+            // throw error
+            error = { message: 'Fill in all fields' };
+            this.setState({ errors: errors.concat(error) });
+            return false;
+        } else if (!this.isPasswordValid(this.state)) {
+            // throw error
+            error = { message: 'Password is invalid' }
+            this.setState({ errors: errors.concat(error) });
+            return false;
+        } else {
+            // form is valid
+            return true;
+        }
+    }
+
+    isFormEmpty = ({username, email, password, passwordConfirmation}) => {
+        return !username.length || !email.length || !password.length ||
+        !passwordConfirmation.length;
+    }
+
+    isPasswordValid = ({ password, passwordConfirmation }) => {
+        if(password.length < 6 || passwordConfirmation < 6) {
+            return false;
+        } else if (password !== passwordConfirmation) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    displayErrors = errors => errors.map((error, i) => <p key={i}>{error.message}</p>);
 
     handleChange = event => {
         this.setState({
@@ -19,25 +57,27 @@ class Register extends React.Component {
     };
 
     handleSubmit = event => {
-        event.preventDefault();
-        firebase
-        .auth()
-        .createUserWithEmailAndPassword(this.state.email, this.state.password)
-        .then(createdUser => {
-            console.log(createdUser);
-        })
-        .catch(err => {
-            console.error(err);
-        });
+        if (this.isFormValid()) {
+            event.preventDefault();
+            firebase
+            .auth()
+            .createUserWithEmailAndPassword(this.state.email, this.state.password)
+            .then(createdUser => {
+                console.log(createdUser);
+            })
+            .catch(err => {
+                console.error(err);
+            });
+        }
     };   
 
     render() {
-        const { username, email, password, passwordConfirmation } = this.state;
+        const { username, email, password, passwordConfirmation, errors } = this.state;
         return (
             <Grid textAlign="center" verticalAlign="middle" className="app">
-                <Grid.Column style={{ maxWidth: 450 }}>
-                    <Header as="h2" icon color="yellow" textAlign="center">
-                        <Icon name="comment outline" color="yellow" />
+                <Grid.Column style={{ maxWidth: 350 }}>
+                    <Header as="h2" icon color="green" textAlign="center">
+                        <Icon name="comment outline" color="green" />
                         Register for ChatMap
                     </Header>
                     <Form onSubmit={this.handleSubmit} size="large">
@@ -74,9 +114,15 @@ class Register extends React.Component {
                                 onChange={this.handleChange} 
                                 value={passwordConfirmation} 
                                 type="password" />
-                            <Button color="yellow" fluid size="large">Submit</Button>
+                            <Button color="green" fluid size="large">Submit</Button>
                         </Segment>
                     </Form>
+                    { errors.length > 0 &&  (
+                        <Message error>
+                            <h3>Error</h3>
+                            {this.displayErrors(errors)}
+                        </Message>
+                    )}
                     <Message>Already a user? <Link to="/login">Login</Link></Message>
                 </Grid.Column>
             </Grid>
